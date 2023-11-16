87e130f5c04c984d1500bfb87904d86629065379
Felipeln <76855578+felipeln@users.noreply.github.com>
2023-11-15 22:34:47 -0300

    useEffect to make an search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 const myList = [
   {id: 0, value: 'Apple'},
   {id: 1, value: 'Banana'},
@@ -5,11 +7,49 @@ const myList = [
 ]
 
 function App() {
+
+
+  const [products, setProducts] = useState(myList)
+  const [search, setSearch] = useState('')
+
+  useEffect(
+    () =>{
+
+      // caso search seja vazio, não execute o codigo abaixo.
+      if(search != ''){
+        // to filtrando a minha lista original
+      // verificando se o item da lista foi pesquisado ou seja inclui o valor da variável search
+      // caso tenha sido pesquisado, retorne para essa nova lista.
+        const newList = myList.filter((item) => {
+          return item.value.toLowerCase().includes(search.toLowerCase())
+        })
+        // atualize o valor do array de list para apenas os itens que estão incluidos em search.
+        setProducts(newList)
+        }
+        else{
+          // caso não tenha pesquisa, deixe as listas normais
+          // no caso vamos atualizar o valor de lista para o padrão.
+          setProducts(myList)
+        }
+      }
+      
+      
+     
+     
+    // parametro que diz quando o use effect deve ser executado.
+    // No caso, sempre que o valor de search for alterado.
+    , [search]
+  )
+
   return (
     <>
     <div>
       <h1>Utilizando efeitos colaterais</h1>
-      {myList.map((item) => {
+      
+      {/* o valor do input sera o da variável pesquisa, que pegara o valor digitado no input sempre que ele for alterado para atualizar o valor da variável pesquisa. */}
+      <input value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search your fruit here."/> 
+
+      {products.map((item) => {
         return (
           <div key={item.id}>
             <h2>{item.value}</h2>
